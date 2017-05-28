21a4f69f7ed6b1597061b08e37095b24058e8abd
Rick Mark <rickmark@outlook.com>
2017-05-27 23:13:00 -0700

    Use admin namespace

--- app/models/locale.rb
+++ app/models/locale.rb
@@ -2,7 +2,7 @@ class Locale
   attr_accessor :name, :id, :label
 
   def self.all(connection)
-    locales = connection.get('/v1/locales')
+    locales = connection.get('/v1/admin/locales')
 
     locales.body['locales'].map do |locale|
       Locale.new locale
