11a6b4a7f9996ff20373e990abf3a40248475a1b
Rick Mark <rickmark@outlook.com>
2017-06-09 20:47:29 -0400

    support audiences

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,7 @@
 class Person < ApplicationModel
-  attr_accessor :page_id, :order, :global, :like_required, :connection, :display_name, :locale, :created_at, :updated_at, :name, :name_override, :facebook_id, :facebook_graph, :facebook_access_token, :facets, :requires_user_token, :hidden, :photo_id, :cover_photo_id
+  attr_accessor :page_id, :order, :global, :like_required, :connection, :display_name, :locale, :created_at,
+                :updated_at, :name, :name_override, :facebook_id, :facebook_graph, :facebook_access_token,
+                :facets, :requires_user_token, :hidden, :photo_id, :cover_photo_id, :audience_id
 
   def self.all(connection)
     people = connection.get('/v1/admin/people')
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,7 +2,7 @@ class Venue < ApplicationModel
   attr_accessor :locale_id, :hidden, :order, :distance_tolerance, :location_id,
                 :page_id, :connection, :name, :facebook_id, :google_place_id, :display_name,
                 :event_count, :created_at, :updated_at, :name_override, :facebook_graph, :facebook_access_token,
-                :facets, :requires_user_token, :photo_id, :cover_photo_id, :locale
+                :facets, :requires_user_token, :photo_id, :cover_photo_id, :locale, :audience_id
 
   def self.all(connection, locale_id)
     url = locale_id ? "/v1/admin/locales/#{locale_id}/venues" : "/v1/admin/venues"
